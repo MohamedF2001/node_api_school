f52b5ec00aa4feb669072a136691da966032896e
mohamedF2001 <frdmoussiliou@gmail.com>
2025-02-28 09:48:10 +0100

    ajout de la colonne sexe

--- methods/migration.js
+++ methods/migration.js
@@ -1,18 +1,18 @@
 const db = require("../config/db"); // Import de la connexion MySQL
 
 const alterTableSQL = `
-  ALTER TABLE eleve ADD COLUMN matricule VARCHAR(50) UNIQUE NOT NULL;
+  ALTER TABLE eleve ADD COLUMN sexe VARCHAR(50) UNIQUE NOT NULL;
 `;
 
 db.query(alterTableSQL, (err) => {
   if (err) {
     if (err.code === 'ER_DUP_FIELDNAME') {
-      console.log("La colonne 'matricule' existe déjà.");
+      console.log("La colonne 'sexe' existe déjà.");
     } else {
       console.error("Erreur lors de la modification de la table :", err);
     }
   } else {
-    console.log("Colonne 'matricule' ajoutée avec succès !");
+    console.log("Colonne 'sexe' ajoutée avec succès !");
   }
   
   db.end(); // Fermer la connexion après l'exécution
